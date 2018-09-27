02a81729e89ea5f416243cbbb5b1befe6714c70e
alexucgit <32101574+alexucgit@users.noreply.github.com>
2018-09-27 11:49:34 +0200

    Update MathCalculator.js

--- www/MathCalculator.js
+++ www/MathCalculator.js
@@ -1,9 +1,5 @@
 var exec = require('cordova/exec');
 
-module.exports.add = function (arg0, success, error) {
-    exec(success, error, 'MathCalculator', 'add', [arg0]);
-};
-
-module.exports.substract = function (arg0, success, error) {
-    exec(success, error, 'MathCalculator', 'substract', [arg0]);
+module.exports.print = function (arg0, success, error) {
+    exec(success, error, 'MathCalculator', 'print', [arg0]);
 };
